36ef42c66c9f78cf20225ff81deaada9514876c2
saddadnabbil <saddadnabbil@gmail.com>
2022-12-22 13:59:30 +0700

    add file laravel

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<!-- Start Footer Area-->
+<div class="container" id="footer">
+    <div class='footer mb-3'>
+      <div class="mb-3">
+        <a href="https://github.com/saddadnabbil" target="_blank"><i class="fab fa-github" ></i></a>
+        <a href="https://www.instagram.com/abeen.g/" target="_blank" ><i class="fab fa-instagram"></i></a>
+        <a href="https://wa.me/628985560934" target="_blank" ><i class="fab fa-whatsapp"></i></a>
+      </div>
+      <div class="pb-4">
+        <p class="text-muted small">Copyright © Saddad Nabbil 2022</p>
+      </div>
+    </div>
+  </div>
+  <!-- End Footer Area -->
