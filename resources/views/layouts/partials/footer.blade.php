279c73999dfb8aaafd1658ad53fbe3e0638a80f9
saddadnabbil <saddadnabbil@gmail.com>
2022-12-26 11:30:37 +0700

    delete instagram, add linkedin

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,7 +3,7 @@
     <div class='footer mb-3'>
       <div class="mb-3">
         <a href="https://github.com/saddadnabbil" target="_blank"><i class="fab fa-github" ></i></a>
-        <a href="https://www.instagram.com/abeen.g/" target="_blank" ><i class="fab fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/saddadnabbil/"><i class="fab fa-linkedin-in"></i></a>
         <a href="https://wa.me/628985560934" target="_blank" ><i class="fab fa-whatsapp"></i></a>
       </div>
       <div class="pb-4">
